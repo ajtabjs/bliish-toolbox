49e28d18185d3ad9755ba5db37a0c596cc8a570b
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-24 22:27:00 +0000

    add comment support

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def post():
 def bliip():
     response = requests.post(bliips, json=bliip_data, headers={"cookie": f"sb-prkqirdzadljdpkrvjvz-auth-token={tokenprompt};", "origin": "https://bliish.com", "referer": "https://bliish.com"})
     print(response.text)
-options = ["1. make a post", "2. bliip a user", "3. get posts from someone's wall (json response, no parsing yet :P)", "4. mass bliip a set of users", "5. exit"]
+options = ["1. make a post", "2. comment on a post", "3. bliip a user", "4. get posts from someone's wall (json response, no parsing yet :P)", "5. mass bliip a set of users", "6. exit"]
 title = "welcome to bliish toolbox! made by aj"
 
 option, index = pick(options, title)
@@ -31,18 +31,23 @@ print(option)
 if index == 0:
     prompt = input("what do you want to post? ")
     post()
-elif index == 1:
+elif index == 2:
     bliipprompt = input("username to bliip? is rate limited lol: ")
     bliip_data["handle"] = bliipprompt
     bliip()
-elif index == 2:
+elif index == 1:    
+    postid = input("post id to comment on? (obtained via url so https://blii.sh/p/QZQXftQz would be an example)")
+    comment = input("thing to say: ")
+    postdata["body"] = comment
+    response = requests.post(f"https://bliish.com/api/v1/posts/{postid}/comments", json=postdata, headers={"cookie": f"sb-prkqirdzadljdpkrvjvz-auth-token={tokenprompt};", "origin": "https://bliish.com", "referer": "https://bliish.com"})
+elif index == 3:
     walls = input("username to get posts from? ")
     response = requests.get(f"https://bliish.com/api/v1/profiles/{walls}/wall?fresh=1&limit=20", headers={"cookie": f"sb-prkqirdzadljdpkrvjvz-auth-token={tokenprompt};", "origin": "https://bliish.com", "referer": "https://bliish.com"})
     print(response.text)
-elif index == 3:
-    users = input(" usernames to bliip? (comma separated no spaces): ")
+elif index == 4:
+    users = input("usernames to bliip? (comma separated no spaces): ")
     for user in users.split(","):
         bliip_data["handle"] = user.strip()
         bliip()
-elif index == 4:
+elif index == 5:
     sys.exit()
