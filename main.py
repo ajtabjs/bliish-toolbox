06948451686bdf95c72f7ec660b3d372c0159fcd
aj <134237782+ajtabjs@users.noreply.github.com>
2026-04-24 22:05:09 +0000

    add title

--- main.py
+++ main.py
@@ -23,7 +23,9 @@ def bliip():
     response = requests.post(bliips, json=bliip_data, headers={"cookie": f"sb-prkqirdzadljdpkrvjvz-auth-token={tokenprompt};", "origin": "https://bliish.com", "referer": "https://bliish.com"})
     print(response.text)
 options = ["1. make a post", "2. bliip a user", "3. get posts from someone's wall", "4. exit"]
-option, index = pick(options)
+title = "welcome to bliish toolbox! made by aj"
+
+option, index = pick(options, title)
 print(option)
 if index == 0:
     prompt = input("what do you want to post? ")
